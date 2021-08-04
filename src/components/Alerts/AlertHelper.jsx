8d7526608b5bd1fdb709748e4595668af41ffe25
fperez-glo <facundoperez6@gmail.com>
2021-08-04 19:57:39 -0300

    Agregados

--- src/components/Alerts/AlertHelper.jsx
+++ src/components/Alerts/AlertHelper.jsx
@@ -1,26 +1,29 @@
-import swal from 'sweetalert'
+import swal from 'sweetalert2'
 
 export default class AlertHelper {
     static success(message, timeOut= 2000, title= "", buttonTitle = "Confirmar") {
-        swal({
+        swal.fire({
             title: title,
             icon: "success",
             text: message,
             timer : timeOut,
             button: buttonTitle,
+            showConfirmButton: false,
         })
       }
       static info(message, timeOut= 2000, title= "", buttonTitle = "Confirmar") {
-        swal({
+        swal.fire({
             title: title,
             icon: "info",
             text: message,
             timer : timeOut,
             button: buttonTitle,
+            showConfirmButton: false,
         })
+        
       }
       static error(message, timeOut= 2000, title= "", buttonTitle = "Confirmar") {
-        swal({
+        swal.fire({
             title: title,
             icon: "error",
             text: message,
